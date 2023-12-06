b226b87c3cfecb0c2261f4a33cf44bb4ae3361d7
AlexisOl <oleoalexis@gmail.com>
2023-12-06 15:19:12 -0600

    agregando funcionalidades del parser

--- Backend/Parser.py
+++ Backend/Parser.py
@@ -1,12 +1,73 @@
 from ply.yacc import  yacc
 from Lexer import tokens, lexer, errores, find_column
+### establecer precedencias 
+
+precedence = (
+    ('left', 'MAS','MENOS'),
+    ('left', 'POR','DIVISION'),
+    ('left', 'COMPARACION','DISTINTO','MENOR_O_IGUAL_QUE', 'MAYOR_O_IGUAL_QUE'),
+    ('left', 'OR','AND'),
+    ('left', 'PARENTESIS_IZQ','PARENTESIS_DER')
+);
+
 
 ## ahora el parser general
+def p_init(t):
+    '''
+    init : instrucciones
+    '''
+    t[0] = t[1]
 
 
+def p_instruccionesListado(t):
+    '''
+    instrucciones : instrucciones instruccion
+    '''
+    ## aqui si existe la instruccion
+    if (t[2]!= ""):
+        t[1].append(t[2])
+    t[0] = t[1]  # Si t[1] no es una lista, crea una nueva lista con los elementos
 
 
+def p_instruccionSimple(t):
+    '''
+    instrucciones : instruccion 
+    '''
+    if t[1] =="":
+        t[0]=[]
+    else :
+        ### no se te olvie apilar como arreglo y no como objeto
+        t[0]= [t[1]]
 
+def instruccionGeneral(t):
+    '''
+    instruccion : CREATE
+                | USE 
+    '''
+##############
+##############
+
+
+##############
+####AHORA PARA LOS OPERADORES##
+def p_expresiones(t):
+    '''
+    expresion : expresion MAS expresion
+            |  expresion MENOS expresion
+            |  expresion POR expresion
+            |  expresion DIVISION expresion
+    '''
+    if t[2] == '+':
+        t[0] = t[1]+t[3]
+    elif t[2]=='-':
+        t[0]=t[1]-t[3]
+    elif t[2]=="*":
+        t[0]= t[1]*t[3]
+    elif t[2]=='/':
+        t[0] = t[1]/t[3]
+
+##############
+##############
 # Parser ESTO PARA EL XML
 def p_element(p):
     '''element : TAGABIERTO attributes TAGCERRADO
