5db69b29472d37fa10ebf67d978ddcd6878d6991
AlexisOl <oleoalexis@gmail.com>
2023-12-06 21:43:28 -0600

    ingreso de lo que llevaba del parser para rectigicar

---
+++ Backend/src/expresiones/primitivos.py
@@ -0,0 +1,15 @@
+from abstract.abtsractas import Abstract
+
+
+class primitivos(Abstract):
+    def __init__(self, fila, columna, valor, tipo):
+        self.valor = valor
+        self.tipo = tipo
+        super().__init__(fila, columna)
+
+    def interpretar(self, arbol, tabla):
+        ## lo que hace de interfaz regresa el valor
+        return self.valor
+    
+    def getTipo(self):
+        return self.tipo
