2aa2926b2423f0c323f8b98decd3a17e187e7157
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2023-12-08 15:52:32 -0600

    resolviendo bug code-editor

--- Frontend/XSQL/src/main.ts
+++
@@ -1,9 +0,0 @@
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/markdown/markdown';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
