bbd4793d6a781a9e592457ccea63d103392a0e78
AlexisOl <oleoalexis@gmail.com>
2023-12-28 22:23:12 -0600

    nueva rama

--- Backend/src/instrucciones/insert/insert.py
+++ Backend/src/instrucciones/insert/insert.py
@@ -19,7 +19,7 @@ class insertInstruccion(Abstract):
         nombre = self.nombreTabla
         nuevosParametros =[]
         nuevosAtributos = []
-
+ 
 
         ### obtencion de archivps
         Estructura.load();
